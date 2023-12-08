8257d826cfaf625005f76b7957ce3f1c7062f44d
Eliejah <eliejahwalker@gmail.com>
2023-12-07 23:41:31 -0600

    Directories Backup

--- backup.py
+++ backup.py
@@ -2,25 +2,39 @@ import os
 import argparse
 import sys
 import shutil
+from datetime import date
+import pdb
 
+# Initialize the argument parser
 parser = argparse.ArgumentParser()
 
+# Get the current date in a specific format
+today = date.today()
+date_format = today.strftime("%d_%b_%Y_")
 
+
+# Function for backing up individual files
 def file_backup(file, src, dest):
+    # Define command line arguments for file backup
     parser.add_argument('--file', nargs='+', required=True, help='input file(s)')
     parser.add_argument('--src', nargs=1, required=True, help='directory of input file(s)')
     parser.add_argument('--dest', nargs=1, required=True, help='Intended destination for file')
 
+    # Parse the command line arguments
     args = parser.parse_args(['--file', file, '--src', src, '--dest', dest])
 
-    user_file = args.file
+    # Extract user inputs from parsed arguments
+    user_file = args.file[0]
+    user_file = user_file.split(' ')
     src_directory = args.src[0]
     dest_directory = args.dest[0]
 
+    # pdb.set_trace() *debugging via command-line*
+    # Loop through each file and perform backup
     for files in user_file:
         source_path = os.path.join(src_directory, files)
         try:
-            backup_dest_1 = shutil.copy2(source_path, os.path.join((dest_directory + '_copy_of_' + files)))
+            backup_dest_1 = shutil.copy2(source_path, os.path.join((dest_directory + "_" + date_format + files)))
         except FileNotFoundError:
             print(" %s Not Found in src directory " % files)
         try:
@@ -31,19 +45,33 @@ def file_backup(file, src, dest):
     print("file move successful")
 
 
+# Function for backing up entire directories
 def dir_backup(directory, src, dest):
+    # Define command line arguments for directory backup
     parser.add_argument('--dir', nargs=1, required=True, help='Directory folder(s)')
     parser.add_argument('--src', nargs=1, required=True, help='directory of input folders')
     parser.add_argument('--dest', nargs=1, required=True, help='Intended destination for folder')
 
+    # Parse the command line arguments
     args = parser.parse_args(['--dir', directory, '--src', src, '--dest', dest])
-    user_folder = args.directory
+    user_folder = args.dir[0]
     src_directory = args.src[0]
     dest_directory = args.dest[0]
 
+    # pdb.set_trace() *debugging via command-line*
+    # Try to copy the entire directory to the destination
+    try:
+        shutil.copytree(src_directory, os.path.join(dest_directory + '\\', user_folder))
+    except FileNotFoundError:
+        print(" Cannot find the path specified, re-check source destination")
+    except FileExistsError:
+        print(" The folder in dest_directory already exists, if not, re-check dest_directory location")
+    else:
+        print("File move successful")
     return 0
 
 
+# Main loop for user interaction
 while True:
     userinput = input('would you like to backup a file or directory: ').lower()
 
